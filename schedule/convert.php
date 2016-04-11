35a05150609cd4c958621faa1129c337c7b8292d
CocoaBrew <CocoaBrew@github.com>
2016-04-11 11:01:02 -0500

    Update 4/11.1
    Added work from the weekend.

--- schedule/convert.php
+++ schedule/convert.php
@@ -29,129 +29,132 @@
   if (!file_exists($destDir)):
     mkdir($destDir);
     chmod($destDir, 0733);
+  endif;
+  
+  # Get names of tutors for a course
+  $query = "select t.name, t.education, t.work_hrs 
+    from tutors as t inner join course_for_tutor as c
+    where t.id = c.id and c.course = :course";
+  $stmt = $db->prepare($query);
+  $stmt->bindParam(':course', $course, PDO::PARAM_STR);
+  $stmt->execute();
+  $names = $stmt->fetchAll();
 
-    # Get names of tutors for a course
-    $query = "select t.name, t.education, t.work_hrs 
-      from tutors as t inner join course_for_tutor as c
-      where t.id = c.id and c.course = :course";
+  # Loop through all names to create each CSV file
+  foreach($names as $name):
+    $result = "nochange";
+    $nameList = explode('+', $name[0]);
+    $fullname = $nameList[0] . $nameList[1];
+    $ed = $name[1];
+    $hrs = $name[2];
+    $destFile = $destDir . '/' . $fullname . $hrs . $ed . '.csv';
+    touch($destFile);
+    chmod($destFile, 0606);
+
+    $query = "select sbusy, mbusy, tbusy, wbusy, rbusy, fbusy, 
+      spref, mpref, tpref, wpref, rpref, fpref 
+      from available as a inner join tutors as t
+      where a.id = t.id and t.name = :name";
     $stmt = $db->prepare($query);
-    $stmt->bindParam(':course', $course, PDO::PARAM_STR);
+    $stmt->bindParam(':name', $name[0], PDO::PARAM_STR);
     $stmt->execute();
-    $names = $stmt->fetchAll();
+    $days = $stmt->fetchAll();
+    $days = $days[0];
 
-    # Loop through all names to create each CSV file
-    foreach($names as $name):
-      $nameList = explode('+', $name[0]);
-      $fullname = $nameList[0] . $nameList[1];
-      $ed = $name[1];
-      $hrs = $name[2];
-      $destFile = $destDir . '/' . $fullname . $hrs . $ed . '.csv';
-      touch($destFile);
-      chmod($destFile, 0606);
+    $sbusy = explode('/', $days['sbusy']);
+    $mbusy = explode('/', $days['mbusy']);
+    $tbusy = explode('/', $days['tbusy']);
+    $wbusy = explode('/', $days['wbusy']);
+    $rbusy = explode('/', $days['rbusy']);
+    $fbusy = explode('/', $days['fbusy']);
+    $spref = explode('/', $days['spref']);
+    $mpref = explode('/', $days['mpref']);
+    $tpref = explode('/', $days['tpref']);
+    $wpref = explode('/', $days['wpref']);
+    $rpref = explode('/', $days['rpref']);
+    $fpref = explode('/', $days['fpref']);
+
+    $outFile = fopen($destFile, 'w');
+
+    $time = '09:00';
+    # Loop through hours 9am to 9pm, including half hours
+    for ($i = 9; $i <= 21; $i = $i + .5):
+      # For each half hour, if the tutor is busy, that time 
+      # has a '-1'. If he prefers that half hour, the time 
+      # has a '1'. For all other times, the time is marked '0'.
+      $csvRow = array();
+      
+      $csvRow[0] = $time;
+          
+      if (in_array($time, $sbusy)):
+        $csvRow[1] = -1;
+      elseif (in_array($time, $spref)):
+        $csvRow[1] = 1;
+      else:
+        $csvRow[1] = 0;
+      endif;
   
-      $query = "select sbusy, mbusy, tbusy, wbusy, rbusy, fbusy, 
-        spref, mpref, tpref, wpref, rpref, fpref 
-        from available as a inner join tutors as t
-        where a.id = t.id and t.name = :name";
-      $stmt = $db->prepare($query);
-      $stmt->bindParam(':name', $name[0], PDO::PARAM_STR);
-      $stmt->execute();
-      $days = $stmt->fetchAll();
+      if (in_array($time, $mbusy)):
+        $csvRow[2] = -1;
+      elseif (in_array($time, $mpref)):
+        $csvRow[2] = 1;
+      else:
+        $csvRow[2] = 0;
+      endif;
   
-      $sbusy = explode('/', $days[0]);
-      $mbusy = explode('/', $days[1]);
-      $tbusy = explode('/', $days[2]);
-      $wbusy = explode('/', $days[3]);
-      $rbusy = explode('/', $days[4]);
-      $fbusy = explode('/', $days[5]);
-      $spref = explode('/', $days[6]);
-      $mpref = explode('/', $days[7]);
-      $tpref = explode('/', $days[8]);
-      $wpref = explode('/', $days[9]);
-      $rpref = explode('/', $days[10]);
-      $fpref = explode('/', $days[11]);
+      if (in_array($time, $tbusy)):
+        $csvRow[3] = -1;
+      elseif (in_array($time, $tpref)):
+        $csvRow[3] = 1;
+      else:
+        $csvRow[3] = 0;
+      endif;
   
-      $outFile = fopen($destFile, 'w');
+      if (in_array($time, $wbusy)):
+        $csvRow[4] = -1;
+      elseif (in_array($time, $wpref)):
+        $csvRow[4] = 1;
+      else:
+        $csvRow[4] = 0;
+      endif;
   
-      $time = '09:00';
-      # Loop through hours 9am to 9pm, including half hours
-      for ($i = 9; $i <= 21; $i = $i + .5):
-        # For each half hour, if the tutor is busy, that time 
-        # has a '-1'. If he prefers that half hour, the time 
-        # has a '1'. For all other times, the time is marked '0'.
-        $csvRow = array();
-        
-        $csvRow[0] = $time;
-            
-        if (in_array($time, $sbusy)):
-          $csvRow[1] = -1;
-        elseif (in_array($time, $spref)):
-          $csvRow[1] = 1;
-        else:
-          $csvRow[1] = 0;
-        endif;
-    
-        if (in_array($time, $mbusy)):
-          $csvRow[2] = -1;
-        elseif (in_array($time, $mpref)):
-          $csvRow[2] = 1;
-        else:
-          $csvRow[2] = 0;
-        endif;
-    
-        if (in_array($time, $tbusy)):
-          $csvRow[3] = -1;
-        elseif (in_array($time, $tpref)):
-          $csvRow[3] = 1;
-        else:
-          $csvRow[3] = 0;
-        endif;
-    
-        if (in_array($time, $wbusy)):
-          $csvRow[4] = -1;
-        elseif (in_array($time, $wpref)):
-          $csvRow[4] = 1;
-        else:
-          $csvRow[4] = 0;
-        endif;
-    
-        if (in_array($time, $rbusy)):
-          $csvRow[5] = -1;
-        elseif (in_array($time, $rpref)):
-          $csvRow[5] = 1;
-        else:
-          $csvRow[5] = 0;
-        endif;
-    
-        if (in_array($time, $fbusy)):
-          $csvRow[6] = -1;
-        elseif (in_array($time, $fpref)):
-          $csvRow[6] = 1;
-        else:
-          $csvRow[6] = 0;
-        endif;
-    
-        # print availability at current time
-        fputcsv($outFile, $csvRow);
-    
-        if (($i / .5) % 2 == 0):
-          $time = substr_replace($time, "30", 3, 2);
-        else:
-          $newHr = $i + 1;
-          $time = $newHr . ":00";
-          if (strlen($time) < 5):
-            $time = str_pad($time, 5, '0', STR_PAD_LEFT);
-          endif;
-        endif;
-    
-      endfor;
+      if (in_array($time, $rbusy)):
+        $csvRow[5] = -1;
+      elseif (in_array($time, $rpref)):
+        $csvRow[5] = 1;
+      else:
+        $csvRow[5] = 0;
+      endif;
   
-      fclose($outFile);
+      if (in_array($time, $fbusy)):
+        $csvRow[6] = -1;
+      elseif (in_array($time, $fpref)):
+        $csvRow[6] = 1;
+      else:
+        $csvRow[6] = 0;
+      endif;
   
-    endforeach;
+      # print availability at current time
+      fputcsv($outFile, $csvRow);
+  
+      if (($i / .5) % 2 == 0):
+        $time = substr_replace($time, "30", 3, 2);
+      else:
+        $newHr = strval(substr($time, 0, 2) + 1);
+        $time = $newHr . ":00";
+        if (strlen($time) < 5):
+          $time = str_pad($time, 5, '0', STR_PAD_LEFT);
+        endif;
+      endif;
+  
+    endfor;
+
+    fclose($outFile);
 
     $result = "created";
-  endif;
+  endforeach;
+
+  //$result = "created";
 
   echo $result;
 
--- schedule/manager.js
+++ schedule/manager.js
@@ -9,16 +9,19 @@ function gebi(item)
 
 window.onload = function() 
 {
-  var createButtons = document.getElementsByClassName("creator");
+  /*var createButtons = document.getElementsByClassName("creator");
   var schedIds = [];
   for (creator in createButtons)
   {
-    document.getElementById('response').innerHTML = "mozzstix";
     var courseName = creator.id.trim();
-    creator.onclick = makeSchedule(courseName);
-  }
-  document.querySelectorAll(schedIds);
+    schedIds.push(courseName);
+  }*/
   document.getElementById('response').innerHTML = "brownies";
+  
+  document.getElementById('HowardMath').addEventListener("click", function() 
+    {
+      makeSchedule(this.id);
+    });
 }
 
 function makeSchedule(course) 
@@ -35,13 +38,14 @@ function makeSchedule(course)
 function toCSV(course) 
 { 
   var xhr = new XMLHttpRequest();
-  xhr.open("POST", "convert.php", true);
+  var success = false;
+  xhr.open("POST", "convert.php", false);
   xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
   var param = "coursename=" + course;
   xhr.send(param);
+  
   var response = xhr.responseText.trim();
-  document.getElementById('response').innerHTML = "888GETPIES";
-  var success = false;
+  document.getElementById('response').innerHTML = response;
   if (response == "created")
   {
     success = true;
@@ -54,7 +58,7 @@ function writeSchedule(course)
 {
   document.getElementById('response').innerHTML = "cocoaBerryLimefrosting";
   var xhr = new XMLHttpRequest();
-  xhr.open("POST", "schedule.py", true);
+  xhr.open("POST", "schedule.py", false);
   xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
   var param = "course=" + course;
   xhr.send(param);
@@ -62,23 +66,7 @@ function writeSchedule(course)
   gebi('response').innerHTML = xhr.responseText;
   
   
-  /*if (!fileExists)
-  {
-    scheduleMakingCode
-  }
-  else
-  {
-    redirectToExistingSchedule
-  }*/
-  
-  /* for each tutor
-  {
-    tutorThread = new Thread(tutorName, hours, edLevel = PRIORITY)
-    tutorThread.run() -> schedule()
-  }*/
-  
-  /* finalSched[12hrs][6days] each with '' 
-  sendBackSchedArray (or sendBackSchedPage)
+  /*
   finalSchedValuesOutputToFormattedHtmlDoc() //htmlDoc w draggable entries?
   */
 }
